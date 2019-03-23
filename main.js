259d29a4a8f9d88b4c5b3b4348193561168697df
seiya hida <microphone1134@gmail.com>
2019-03-23 19:00:08 +0900

    s

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-$(function () {
-$(".header_sp_nav").click(function () {
-$(".nav1").slideToggle();
-})
-$(window).scroll(function () {
-  $("h1").animate({
-    'fontSize': '5px'
+$(function() {
+  $(".header_sp_nav").click(function() {
+    $(".nav1").slideToggle()
+  })
+  $(window).scroll(function() {
+
+    if ($(window).scrollTop() === 0) {
+       $("h1").addClass("active")
+$("h1").removeClass("disable")
+     } else {
+       $("h1").removeClass("active")
+$("h1").addClass("disable")
+
+    }
+
     
-  });
 
-
-});
+  })
 })
